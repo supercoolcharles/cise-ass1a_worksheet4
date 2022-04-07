0876381094f6b7a8e4919d27f666f17d7455b8fc
supercoolcharles <80949791+supercoolcharles@users.noreply.github.com>
2022-04-07 17:55:51 +1200

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
 import React from "react";
 import{
-  BrowserRouter as Router
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Routes,
+  Navigate,
 }from "react-router-dom";
+
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+
 const App = () =>  {
     return (
         <Router>
         <div>
           <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
             <ul className="header">
-                <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+            <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
+            <Routes>
+          <Route exact path="/" element={<Home />}/>
+            <Route  path="/SEPractice" element={<SEPractice/>}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle/>}/>
+            <Route exact path="*" element={<NotFoundPage/>}/>
+          </Routes>
           </div>
         </div>
         </Router>
     );
 }
- 
 export default App;
